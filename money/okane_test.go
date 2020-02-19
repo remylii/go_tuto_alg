f30ee6341abe36d3b65d7bdbc3198364169315c9
remyli <rapi.remyli@gmail.com>
2020-02-19 16:14:26 +0900

    update testcase

--- money/okane_test.go
+++ money/okane_test.go
@@ -1,22 +1,44 @@
 package main
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestOkaneSuccess(t *testing.T) {
-  expected_maps := map[string]int{"10000": 1, "5000": 1, "1000": 4, "500": 1, "100": 4, "50": 1, "10": 4, "5": 1, "1": 4}
-  res := okane(19999)
+	cases := okaneSuccessProvidor()
 
-  for k, expected := range expected_maps {
-    value, ok := res[k];
-    if ok == false {
-      t.Fatalf("failed. key %s doesn't exists.", k)
-    }
+	for _, testCase := range cases {
+		res := okane(testCase.Argument)
 
-    if value != expected {
-      t.Fatalf("failed. return value (%d) is not equals %d", value, expected)
-    }
-  }
+		for key, expectedValue := range testCase.Expected {
+			value, ok := res[key]
+			if ok == false {
+				t.Fatalf("failed. key: %s doesn't exists.", key)
+			}
+
+			if value != expectedValue {
+				t.Fatalf("failed. key: `%s` is `%d`. should be returned `%d`.", key, value, expectedValue)
+			}
+		}
+	}
 }
 
+type OkaneCase struct {
+	Argument int
+	Expected map[string]int
+}
+
+func okaneSuccessProvidor() []OkaneCase {
+	cases := []OkaneCase{
+		{
+			Argument: 19999,
+			Expected: map[string]int{"10000": 1, "5000": 1, "1000": 4, "500": 1, "100": 4, "50": 1, "10": 4, "5": 1, "1": 4},
+		},
+		{
+			Argument: 10001,
+			Expected: map[string]int{"10000": 1, "5000": 0, "1000": 0, "500": 0, "100": 0, "50": 0, "10": 0, "5": 0, "1": 1},
+		},
+	}
+
+	return cases
+}
