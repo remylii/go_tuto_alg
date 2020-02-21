9a3ff9b018ebda9e9dd50ba7227a5070c52eae23
remyli <rapi.remyli@gmail.com>
2020-02-21 15:12:22 +0900

    漏れ

--- money/okane_test.go
+++ money/okane_test.go
@@ -38,6 +38,10 @@ func okaneSuccessProvidor() []OkaneCase {
 			Argument: 10001,
 			Expected: map[string]int{"10000": 1, "5000": 0, "1000": 0, "500": 0, "100": 0, "50": 0, "10": 0, "5": 0, "1": 1},
 		},
+		{
+			Argument: 999999,
+			Expected: map[string]int{"10000": 99, "5000": 1, "1000": 4, "500": 1, "100": 4, "50": 1, "10": 4, "5": 1, "1": 4},
+		},
 	}
 
 	return cases
